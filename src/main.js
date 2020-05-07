40cad8dfab4d407f3d5cc85dea48beb52d4b7767
Matheus Solha <mcsolha@gmail.com>
2020-05-07 03:09:01 -0300

    basic search style and functionality

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { createMarker, createInfoWindow } from './js/drawers';
 
-let map;
+let map, autocompleteService;
+let searchInputEl;
+let predictsContainer;
+let sessionToken;
+let predictions;
+let mapMarkerIcon = '<i class="fas fa-map-marker-alt"></i>';
+let subwayIcon = '<i class="fas fa-subway"></i>';
 
 function showUserLocation() {
     if (!navigator.geolocation) {
@@ -35,6 +41,63 @@ function initMap() {
     showUserLocation();
 }
 
+function searchLocation(input) {
+    autocompleteService.getPlacePredictions({
+        input,
+        sessionToken,
+    }, (predictionsResponse, placeServiceStatus) => {
+        predictions = predictionsResponse;
+        renderPredictions();
+    });
+}
+
+function showInput() {
+    searchInputEl.classList.remove('hide');
+}
+
+function renderPredictions() {
+    let ulEl = predictsContainer.firstElementChild;
+    ulEl.innerHTML = '';
+
+    for (const prediction of predictions) {
+        let liString = `<li>${predictionHTML(prediction)}</li>`;
+
+        ulEl.insertAdjacentHTML('beforeend', liString);
+    }
+}
+
+function predictionHTML({ description, types }) {
+    return `<a href="#">${getIcon(types)} <span>${description}</span></a>`;
+}
+
+function getIcon(types) {
+    if (types.includes('train_station') || types.includes('subway_station')) {
+        return subwayIcon;
+    }
+
+    return mapMarkerIcon;
+}
+
 window.onMapsAPILoaded = function() {
+    sessionToken = new google.maps.places.AutocompleteSessionToken();
+    autocompleteService = new google.maps.places.AutocompleteService();
+
     initMap();
+    showInput();
 };
+
+document.addEventListener('DOMContentLoaded', () => {
+    searchInputEl = document.querySelector('.localiza-app__search-input');
+    predictsContainer = document.querySelector('.localiza-app__predicts-container');
+    let searchBarEl = document.querySelector('.localiza-app__search-input');
+
+    searchInputEl.addEventListener('input', (e) => {
+        let value = e.target.value;
+
+        if (!value) {
+            searchBarEl.classList.remove('active-search');
+        }
+        searchBarEl.classList.add('active-search');
+        searchLocation(value);
+    })
+})
