d856f57f3bdd973a543c7980e0a83190b2aac398
Matheus Solha <mcsolha@gmail.com>
2020-05-10 18:23:37 -0300

    createObservable implemented

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -5,3 +5,25 @@ export function isEmptyString(str) {
 export function addMultipleEventsListener(element, events, listener) {
     events.forEach(event => element.addEventListener(event, listener));
 }
+
+export function createObservable(target) {
+    let propertiesCallbacks = {};
+    let observable = new Proxy(target, {
+        set(target, prop, value) {
+            let oldValue = target[prop];
+            target[prop] = value;
+
+            if (propertiesCallbacks[prop]) {
+                propertiesCallbacks[prop](oldValue, value);
+            }
+
+            return true;
+        }
+    });
+
+    observable.observe = function(property, callback) {
+        propertiesCallbacks[property] = callback;
+    }
+
+    return observable;
+}
